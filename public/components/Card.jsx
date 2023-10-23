2b98b4f42fab2ffeaacbfee342671d85bf04aafa
Israel Onoja <Israelonoja21@gmail.com>
2023-10-23 17:26:47 +0200

    pass object as a prop

--- public/components/Card.jsx
+++ public/components/Card.jsx
@@ -4,9 +4,9 @@ import Staricon from "../images/Star 1.png"
 export default function Card(props) {
     
   let badgeText;
-    if(props.openSpots === 0){
+    if(props.card.openSpots === 0){
       badgeText = "SOLD OUT"
-    }else if(props.location === 'Online'){
+    }else if(props.card.location === 'Online'){
       badgeText = "ONLINE"
     }
 
@@ -14,15 +14,15 @@ export default function Card(props) {
   return (
     <div className='card'>
       {badgeText && <div className='card--badge'>{badgeText}</div>}
-      <img src={`./images/${props.img}`} className='card--image'/>
+      <img src={`./images/${props.card.coverImg}`} className='card--image'/>
           <div className='card--stats'>
               <img src={Staricon} alt="star icon" className='card--star'/>
-              <span>{props.rating}</span>
-              <span className='gray'>({props.reviewCount})•</span>
-              <span className='gray'>{props.location}</span>
+              <span>{props.card.stats.rating}</span>
+              <span className='gray'>({props.card.stats.reviewCount})•</span>
+              <span className='gray'>{props.card.location}</span>
           </div>
-          <p className='card--title'>{props.title}</p>
-          <p className='card--price'><span className='bold'>From $ {props.price} </span>/ person</p>
+          <p className='card--title'>{props.card.title}</p>
+          <p className='card--price'><span className='bold'>From $ {props.card.price} </span>/ person</p>
     </div>
   )
 }
